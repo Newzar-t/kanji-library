73cf778fddaf87e5debcc163b5741e9839bcac3a
New <naths.raf@gmail.com>
2025-12-17 17:02:06 +0100

    second commit install supabase

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { createClient } from '@supabase/supabase-js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+class Home {
+  constructor(){
+     this.initSupabase()
+  }
 
-setupCounter(document.querySelector('#counter'))
+initSupabase(){
+  this.supabase = createClient('https://flwtxxzmabehcaktnkme.supabase.co',process.env.SUPABASE_KEY);
+}
+}
